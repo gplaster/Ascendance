01216843c6d85b010b0b97ce20c6e4bcf8b07ff1
gplaster <grant.plaster@yahoo.com>
2015-02-19 09:56:28 -0800

    Jumping fix, anim, rotation
    Fixed jumping through platforms. Added Scotty's animation controls and
    rotations for main character

--- Assets/Scripts/PlatformCollide.cs
+++ Assets/Scripts/PlatformCollide.cs
@@ -4,12 +4,11 @@ using System.Collections;
 public class PlatformCollide : MonoBehaviour {
 
 	GameObject player;
-	PlayerMovement playerMovement;
 	Bounds playerBounds;
 	
 	void Awake() {
 		player = GameObject.FindGameObjectWithTag("Player");
-		playerMovement = player.GetComponent<PlayerMovement>();
+		collider.isTrigger = true;
 	}
 
 	void Update () {
@@ -20,11 +19,12 @@ public class PlatformCollide : MonoBehaviour {
 
 		if (other.tag == "Player") {
 
+			/* If player feet are above top of platform, set collider as trigger */
 			if(playerBounds.min.y >= collider.bounds.max.y)
 			{
 			    collider.isTrigger = false;
 			}
-			else
+			else /* If player feet is below top of platform, ignore collisions with platform */
 			{
 				collider.isTrigger = true;
 			}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,10 +10,15 @@ public class PlayerMovement : MonoBehaviour {
 	public float jumpForce = 400f;
 	public bool canJump = true;
 	Vector3 movement;
+	Vector3 rotation;
 	Rigidbody playerRigidbody;
+
+	public Animator anim;
+	public bool walking;
 
 	void Awake() {
 		playerRigidbody = GetComponent<Rigidbody> ();
+		anim = GetComponent<Animator> ();
 	}
 
 	void FixedUpdate () {
@@ -21,12 +26,24 @@ public class PlayerMovement : MonoBehaviour {
 		float v = Input.GetAxisRaw ("Vertical");
 		float j = Input.GetAxisRaw ("Jump");
 
+		if (h != 0) {
+			walking = true;
+		}
+		else {
+			walking = false;
+		}
+
 		Move (h, j, v);
 	}
 
 	void Move (float h, float j, float v) {
+
 		movement.Set (h, 0f, v);
 		movement = movement.normalized * speed * Time.deltaTime;
+
+		if (h != 0) {
+			transform.forward = new Vector3(-h, 0f, 0f);
+		}
 
 		playerRigidbody.MovePosition (transform.position + movement);
 
@@ -36,10 +53,15 @@ public class PlayerMovement : MonoBehaviour {
 			//playerRigidbody.MovePosition (transform.position + (transform.up * j));
 			playerRigidbody.AddForce (transform.up * jumpForce);
 		}
+
+		anim.SetBool("isWalking", walking);
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other.tag == "Ground" || other.tag == "Platform") {
+		if (other.tag == "Ground") {
+			canJump = true;
+		}
+		else if (other.tag == "Platform" && !other.collider.isTrigger) {
 			canJump = true;
 		}
 	}
